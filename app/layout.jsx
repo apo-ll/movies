39b5faa3fe5bf0af2590dfa2641ac25c6ce90dc2
apo-ll <marsappollo3@gmaill.com>
2024-02-18 05:49:11 +0300

    hdkd

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css';
+import { Navbar } from '@/components/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>{children}</body>
+      <body className={`${roboto.className} bg-black text-white`}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,54 @@
+'use client'
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
-    return (
-        <nav className="px-[70px] py-[30px] text-xl ">
-            <div className="flex container gap-[64px] items-center">
-                <h2>What.To.Watch</h2>
-                <h3 className="text-black bg-white px-[24px] py-[10px] rounded-[100px] font-semibold">Home</h3>
-                <ul className="flex gap-[63px] items-start max-w-[1300px]  ">
-                    {items.map((item, index) => (
-                        <li key={index} className="">{item}</li>
-                    ) )}
-            </ul>
-            </div>
-            
-        </nav>
-    )
-}
+  const pathname = usePathname();
 
-const items = ['Search', 'Movies', 'Tv Shows']
+  return (
+    <nav className="px-[70px] py-[30px] text-[14px] ">
+      <div className="flex container gap-[64px] items-center justify-center">
+        <div className="flex gap-[63px] items-center max-w-[1300px]  ">
+          {items.map((item, index) => (
+            <Link
+              key={index}
+              href={item.link}
+              className={`Link ${
+                pathname === item.link
+                  ? "text-black bg-white px-[16px] py-[6px] rounded-[100px] font-semibold"
+                  : ""
+              }`}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const items = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Search",
+    link: "/Search",
+  },
+  {
+    name: "Movies",
+    link: "/Movies",
+  },
+  {
+    name: "Shows",
+    link: "/Shows",
+  },
+
+  {
+    name: "Library",
+    link: "/Library",
+  },
+];
